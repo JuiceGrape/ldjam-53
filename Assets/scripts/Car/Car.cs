05a694475a6e18c2b83df902df47f0652f1016ca
Bas Mentjot <bmentjot@gmail.com>
2023-04-29 17:21:04 +0200

    Tightened truck controls, added reverse and braking

--- Assets/scripts/Car/Car.cs
+++ Assets/scripts/Car/Car.cs
@@ -9,14 +9,19 @@ public class Car : MonoBehaviour
     [SerializeField] private float baseAccelerationForce = 500f;
     [SerializeField] private float baseBrakingForce = 300f;
     [SerializeField] private float baseSteeringAngle = 25.0f;
+    [SerializeField] private bool accelerateIsBrake = true;
 
     private bool isDriving = false;
     private bool isBraking = false;
+
+    new Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         wheels = GetComponentsInChildren<Wheel>();
-        GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.1f, 0);
+        rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.centerOfMass = new Vector3(0, -0.1f, 0);
     }
 
     // Update is called once per frame
@@ -25,19 +30,14 @@ public class Car : MonoBehaviour
         Accelerate(Input.GetAxisRaw("Vertical"));
         Steer(Input.GetAxisRaw("Horizontal"));
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.Space))
         {
             Brake(1);
-        }
-        else
-        {
-            Brake(0);
         }
     }
 
     public void Steer(float value)
     {
-        Debug.Log(value);
         foreach (Wheel wheel in wheels)
         {
             wheel.SetSteering(baseSteeringAngle * value);
@@ -46,9 +46,39 @@ public class Car : MonoBehaviour
 
     public void Accelerate(float value)
     {
-        foreach (Wheel wheel in wheels)
+        
+        if (accelerateIsBrake)
         {
-            wheel.SetSpeed(baseAccelerationForce * value);
+            var locVel = transform.InverseTransformDirection(rigidbody.velocity);
+            Debug.Log(locVel.z);
+            if ((locVel.z > 0.01 && value < 0) || 
+                (locVel.z < -0.01 && value > 0))
+            {
+                Debug.Log("Braking");
+                Brake(1);
+                foreach (Wheel wheel in wheels)
+                {
+                    wheel.SetSpeed(0);
+                }
+
+            } 
+            else
+            {
+                Debug.Log("Driving");
+                Brake(0);
+                foreach (Wheel wheel in wheels)
+                {
+                    wheel.SetSpeed(baseAccelerationForce * value);
+                }
+            }
+            Debug.Log("------------------");
+        }
+        else
+        {
+            foreach (Wheel wheel in wheels)
+            {
+                wheel.SetSpeed(baseAccelerationForce * value);
+            }
         }
     }
 
