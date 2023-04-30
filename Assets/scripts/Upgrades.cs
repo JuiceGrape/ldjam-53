47dc2bf559339feb61e0541d373abdd359a92eaa
Bas Mentjot <bmentjot@gmail.com>
2023-04-30 23:19:16 +0200

    Tweaked some settings, ready for build

--- Assets/scripts/Upgrades.cs
+++ Assets/scripts/Upgrades.cs
@@ -21,16 +21,16 @@ public class Upgrades : MonoBehaviour
         private set;
     }
 
-    public Upgrade speed = new Upgrade("Max Speed", 10, 10.0f, 2.0f, 1.0f);
-    public Upgrade accell = new Upgrade("Acelleration", 10, 10.0f, 2.0f, 1.0f);
-    public Upgrade brake = new Upgrade("Braking Force", 10, 10.0f, 2.0f, 1.0f);
-    public Upgrade turning = new Upgrade("TurningSpeed", 10, 10.0f, 2.0f, 0.25f);
+    public Upgrade speed = new Upgrade("Max Speed", 10, 10.0f, 1.0f, 0.2f);
+    public Upgrade accell = new Upgrade("Acelleration", 10, 10.0f, 1.0f, 0.5f);
+    public Upgrade brake = new Upgrade("Braking Force", 10, 10.0f, 1.0f, 1.0f);
+    public Upgrade turning = new Upgrade("TurningSpeed", 10, 10.0f, 1.0f, 0.1f);
     //public Upgrade plating = new Upgrade("Armour plating", 10, 10.0f, 2.0f);
 
     public Upgrade spikes = new Upgrade("Bumper Spikes", 1, 100.0f, 1.0f, -1.0f);
 
-    public Upgrade tips = new Upgrade("Tip amount", 10, 10.0f, 2.0f, 1.0f);
-    public Upgrade wages = new Upgrade("Get a raise", 10, 10.0f, 2.0f, 0.5f);
+    public Upgrade tips = new Upgrade("Tip amount", 10, 10.0f, 1.0f, 1.0f);
+    public Upgrade wages = new Upgrade("Get a raise", 10, 10.0f, 1.0f, 0.5f);
     //public Upgrade patience = new Upgrade("Customer patience", 10, 10.0f, 2.0f);
 
     private Dictionary<UpgradeType, Upgrade> upgradeDict;
